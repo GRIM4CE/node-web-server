b13bb02dc729d43b697f0e4e8d2dae5ec466797d
Jon Leibham <jleibham@gmail.com>
2017-01-30 15:41:46 -0600

    fixes

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use((req, res, next) => {
 });
 
 // app.use((req, res, next) => {
-//   res.render('maintence');
+//   res.render('maintenance');
 // });
 
 app.use(express.static('./public'));
